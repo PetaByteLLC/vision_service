fc8d9e7be7c992678d03e715616c829151a077d1
KanHub02 <usmnvk200207@gmail.com>
2024-03-13 18:55:58 +0600

    Modified: execute.py

--- execute.py
+++ execute.py
@@ -3,7 +3,10 @@ from typing import Tuple, Union
 from .ai_service.app_gts import lp_det_reco
 from .ai_service.car_attributes import predict_image
 import sys
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 sys.path.insert(0, "src/web_service/vision_service")
 
@@ -16,6 +19,9 @@ def process_image(img_path: str) -> Union[dict, Tuple]:
     return: A dictionary with data if a car is not detected, or a tuple containing the license plate information and car attributes.
     """
     # car_attributes = predict_image(img_path)
-    lp = lp_det_reco(img_path)
-    print(lp)
-    return lp
+    try:
+        lp = lp_det_reco(img_path)
+        logger.info(f"Recognition result: {lp}")
+        return lp
+    except Exception as e:
+        raise e
